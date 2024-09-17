e43421e4eb8c86aa488f472663577a647b67c25b
Hariokl <sevabu06@gmail.com>
2024-09-17 09:06:00 +0300

    renamed constants

--- clock_time.cpp
+++ clock_time.cpp
@@ -1,67 +1,70 @@
 #include <iostream>
 
-const int kZero = 0;
 const int kOne = 1;
 const int kTwo = 2;
 const int kThree = 3;
 const int kFour = 4;
 const int kFive = 5;
-const int kTen = 10;
-const int kTwelve = 12;
-const int kEighteen = 18;
-const int kTwentyFour = 24;
-const int kSixty = 60;
+
+const int kMinTime = 0;
+const int kMorning = 5;
+const int kModTen = 10;
+const int kHalfADay = 12;
+const int kEvening = 18;
+const int kMaxHours = 24;
+const int kMaxMinutes = 60;
 
 int main() {
-    int hours = kZero;
-    int minutes = kZero;
+    int hours = kMinTime;
+    int minutes = kMinTime;
     std::cin >> hours >> minutes;
+
     // std::cin.good() checks if inputs are of correct types
-    if (!(std::cin.good() && hours >= kZero && hours < kTwentyFour && minutes >= kZero && minutes < kSixty)) {
+    if (!(std::cin.good() && hours >= kMinTime && hours < kMaxHours && minutes >= kMinTime && minutes < kMaxMinutes)) {
         std::cout << "введены недопустимые данные" << std::endl;
         return 1;
     }
 
-    if (hours == kTwelve && minutes == kZero) {
+    if (hours == kHalfADay && minutes == kMinTime) {
         std::cout << "полдень" << std::endl;
         return 0;
-    } else if (hours == kZero && minutes == kZero) {
+    } else if (hours == kMinTime && minutes == kMinTime) {
         std::cout << "полночь" << std::endl;
         return 0;
     }
 
-    std::cout << hours % kTwelve;
+    std::cout << hours % kHalfADay;
 
-    if (hours % kTwelve == kOne) {
+    if (hours % kHalfADay == kOne) {
         std::cout << " час";
-    } else if (hours % kTwelve >= kTwo && hours % kTwelve < kFive) {
+    } else if (hours % kHalfADay >= kTwo && hours % kHalfADay < kFive) {
         std::cout << " часа";
     } else {
         std::cout << " часов";
     }
 
-    if (minutes != kZero) {
+    if (minutes != kMinTime) {
         std::cout << " " << minutes;
-        if (minutes % kTen == kOne) {
+        if (minutes % kModTen == kOne) {
             std::cout << " минута";
-        } else if (minutes % kTen == kTwo || minutes % kTen == kThree || minutes % kTen == kFour) {
+        } else if (minutes % kModTen == kTwo || minutes % kModTen == kThree || minutes % kModTen == kFour) {
             std::cout << " минуты";
         } else {
             std::cout << " минут";
         }
     }
 
-    if (kFive <= hours && hours < kTwelve) {
+    if (kMorning <= hours && hours < kHalfADay) {
         std::cout << " утра";
-    } else if (kTwelve <= hours && hours < kEighteen) {
+    } else if (kHalfADay <= hours && hours < kEvening) {
         std::cout << " дня";
-    } else if (kEighteen <= hours && hours < kTwentyFour) {
+    } else if (kEvening <= hours && hours < kMaxHours) {
         std::cout << " вечера";
     } else {
         std::cout << " ночи";
     }
 
-    if (minutes == kZero) {
+    if (minutes == kMinTime) {
         std::cout << " ровно";
     }
 
