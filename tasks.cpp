8a50d6921dc5f3fdb32d8cced6f08ab019854e54
Hariokl <sevabu06@gmail.com>
2024-10-09 16:45:29 +0300

    fff

--- tasks.cpp
+++ tasks.cpp
@@ -3,32 +3,28 @@
 #include <iostream>
 
 namespace {
-const int kTaskOne = 1;
-const int kTaskTwo = 2;
-const int kTaskThree = 3;
-const int kTaskFour = 4;
+const int kTaskOne = '1';
+const int kTaskTwo = '2';
+const int kTaskThree = '3';
+const int kTaskFour = '4';
 
-const int kTaskFourIntermediateSumCaseOne = 3;
-const int kTaskFourIntermediateSumCaseTwo = 5;
-const int kTaskFourIntermediateSumCaseThree = 10;
-
-const int kPrecision = 6;              //
-const int kSetOutputNumberWidth = 11;  //
+const int kPrecision = 6;
 }  // namespace
 
 void taskOne() {
+    const int kIStep = 5;
     int n = 0;
     int m = 0;
     std::cout << "Введите натуральные числа n и m (m < n):" << '\n';
     std::cin >> n >> m;
 
-    if (m >= n || m < 0) {
+    if (m >= n || m <= 0 || n <= 0) {
         std::cout << "Были введены неправильные данные." << '\n';
         return;
     }
 
     int naturalSum = 0;
-    for (int i = 5; i <= n; i += 5) {
+    for (int i = 0; i <= n; i += kIStep) {
         if (i % m != 0) {
             naturalSum += i;
         }
@@ -37,101 +33,99 @@ void taskOne() {
 }
 
 void taskTwo() {
-    int a = 0;
+    double a = 0.;
     std::cout << "Введите число a:" << '\n';
     std::cin >> a;
 
-    int S = 1;
+    double sum = 1.;
     if (a >= 0) {
-        for (int i = 2; i <= 8; i += 2) {
-            S *= i * i;
+        const int kMinI = 2;
+        const int kMaxI = 8;
+        const int kStepI = 2;
+        for (int i = kMinI; i <= kMaxI; i += kStepI) {
+            sum *= i * i;
         }
-        S -= a;
+        sum -= a;
     } else {
-        for (int i = 3; i <= 9; i += 3) {
-            S *= (i - 2);
+        const int kMinI = 3;
+        const int kMaxI = 9;
+        const int kStepI = 3;
+        const int kTwo = 2;
+        for (int i = kMinI; i <= kMaxI; i += kStepI) {
+            sum *= (i - kTwo);
         }
     }
-    std::cout << "Ответ на задачу: " << S << '\n';
+    std::cout << "Ответ на задачу: " << sum << '\n';
 }
 
 void taskThree() {
-    float Y = .0;
-    float A = .0;
-    float A_last = .0;
-    float S = .0;
-    float S_last = .0;
-    int n = 1;
+    const int kSetOutputNumberWidth = 11;
+    const double kEpsilon = 1e-6;
+    const double kCosPi4 = std::cos(M_PI / 4);
+    const double kMinX = 0.;
+    const double kMaxX = 1.;
+    const double kStepX = 0.2;
 
-    std::cout << '|' << std::setw(kSetOutputNumberWidth) << 'x';
-    std::cout << '|' << std::setw(kSetOutputNumberWidth) << 'Y';
-    std::cout << '|' << std::setw(kSetOutputNumberWidth) << 'S';
-    std::cout << '|' << std::setw(kSetOutputNumberWidth) << 'N' << '|' << '\n';
-    for (float x = .0; x <= 1; x += 0.2) {
-        Y = 1 + x * std::exp(x) * std::cos(M_PI / 4);
+    std::cout << std::setw(kSetOutputNumberWidth) << 'x';
+    std::cout << std::setw(kSetOutputNumberWidth) << 'Y';
+    std::cout << std::setw(kSetOutputNumberWidth) << 'S';
+    std::cout << std::setw(kSetOutputNumberWidth) << 'N' << '\n';
 
-        A = 0;
-        A_last = std::cos(M_PI / 4) * x;
-        S = 1 + std::cos(M_PI / 4) * x;
-        S_last = 1;
-        n = 1;
-        while (S - S_last > 1e-6) {
-            S_last = S;
-            A = A_last / n * x;
-            A_last = A;
-            S += A;
+    double x = kMinX;
+    while (x <= kMaxX) {
+        double Y = 1 + x * std::exp(x) * kCosPi4;
+
+        double summand = kCosPi4 * x;
+        double sum = 1 + kCosPi4 * x;
+        double lastSum = 1;
+        int n = 1;
+        while (sum - lastSum > kEpsilon) {
+            lastSum = sum;
+            summand /= n * x;
+            sum += summand;
             ++n;
         }
-        std::cout << '|' << std::setw(kSetOutputNumberWidth) << x;
-        std::cout << '|' << std::setw(kSetOutputNumberWidth) << Y;
-        std::cout << '|' << std::setw(kSetOutputNumberWidth) << S;
-        std::cout << '|' << std::setw(kSetOutputNumberWidth) << n + 2 << '|' << '\n';
+
+        std::cout << std::setw(kSetOutputNumberWidth) << x;
+        std::cout << std::setprecision(kPrecision) << std::setw(kSetOutputNumberWidth) << Y;
+        std::cout << std::setw(kSetOutputNumberWidth) << sum;
+        std::cout << std::setw(kSetOutputNumberWidth) << n + 1 << std::endl;
+
+        x += kStepX;
     }
 }
 
 void taskFour() {
-    float y = .0;
+    const int kIntermediateSumCaseOne = 3;
+    const int kIntermediateSumCaseTwo = 5;
+    const int kIntermediateSumCaseThree = 10;
+    double y = .0;
     int n = 0;
-    float x = .0;
+    double x = .0;
     std::cout << "Введите числа n (n - натуральное число) и x (|x| < 1):" << '\n';
     std::cin >> n >> x;
 
-    if (n <= 0 || abs(x) >= 1) {
+    if (n <= 0 || std::fabs(x) >= 1.) {
         std::cout << "Были введены неправильные данные." << '\n';
         return;
     }
 
-    bool isRunning = true;
-    char userChoiceToContinue = 'n';
-    while (isRunning) {
-        float A_last = 1.;
-        for (int i = 1; i <= n; ++i) {
-            y += A_last * x / i;
-            A_last = A_last * x * (-1);
-            if (i != n && (i == kTaskFourIntermediateSumCaseOne || i == kTaskFourIntermediateSumCaseTwo || i == kTaskFourIntermediateSumCaseThree)) {
-                std::cout << "Промежуточное значение суммы при n = " << i << ": " << y << '\n';
-            }
-        }
-        std::cout << "Значение искомой суммы: " << y << "\n\n";
-        std::cout << "Хотите продолжить вычислять? (y/n)" << '\n';
-        std::cin >> userChoiceToContinue;
-        if (userChoiceToContinue == 'y') {
-            std::cout << "Введите новое число n:" << '\n';
-            std::cin >> n;
-        } else {
-            isRunning = false;
+    double summand = 1.;
+    for (int i = 1; i <= n; ++i) {
+        y += summand * x / i;
+        summand = summand * x * (-1);
+        if (i != n && (i == kIntermediateSumCaseOne || i == kIntermediateSumCaseTwo || i == kIntermediateSumCaseThree)) {
+            std::cout << "Промежуточное значение суммы при n = " << i << ": " << std::setprecision(kPrecision) << y << '\n';
         }
     }
+    std::cout << "Значение искомой суммы: " << std::setprecision(kPrecision) << y << "\n\n";
 }
 
 int main(int, char**) {
-    std::cout << std::fixed << std::setprecision(kPrecision);
+    char userTaskChoice = '0';
+    char continueExecution = 'y';
 
-    int userTaskChoice = 0;
-    char userChoiceToContinue = 0;
-    bool isRunning = true;
-
-    while (isRunning) {
+    while (continueExecution == 'y') {
         std::cout << "Выберите задачу из списка и напишите её номер для выподнения:" << '\n';
         std::cout << '\t' << "Задача №1 (сумма натуральных чисел на отрезке)" << '\n';
         std::cout << '\t' << "Задача №2 (произведение чисел на заданном отрезке)" << '\n';
@@ -139,10 +133,6 @@ int main(int, char**) {
         std::cout << '\t' << "Задача №4 (вычисление y по формуле)" << '\n';
 
         std::cin >> userTaskChoice;
-        while (userTaskChoice < kTaskOne || userTaskChoice > kTaskFour) {
-            std::cout << "Неправильный ввод, введите ещё раз" << '\n';
-            std::cin >> userTaskChoice;
-        }
 
         std::cout << "Задача №" << userTaskChoice << '\n';
         switch (userTaskChoice) {
@@ -160,16 +150,11 @@ int main(int, char**) {
                 break;
             default:
                 std::cout << "Неправильный ввод" << '\n';
-                return 1;
+                break;
         }
 
         std::cout << "Продолжить работу? (y/n)" << '\n';
-        std::cin >> userChoiceToContinue;
-        if (userChoiceToContinue == 'y') {
-            continue;
-        } else {
-            isRunning = false;
-        }
+        std::cin >> continueExecution;
     }
     return 0;
 }
