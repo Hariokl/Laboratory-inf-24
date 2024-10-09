d9afb28c212529fef899ac7775615909d4608b66
Hariokl <sevabu06@gmail.com>
2024-10-09 12:27:49 +0300

    a

---
+++ a.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include <cmath>
+
+int iterationMethod(int a) {
+    const int N_max = 1e5;
+
+    return 1;
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+#include <a.h>
+
+
+int main(int, char**) {
+    return 0;
+}
