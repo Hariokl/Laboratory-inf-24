f9587d21479f242255c246d542704934dcccb802
Hariokl <sevabu06@gmail.com>
2024-12-06 16:48:11 +0300

    to be aproved

--- solver.cpp
+++ solver.cpp
@@ -1,81 +1,200 @@
-#include <cmath>
 #include "solver.h"
-#include "random"
+
+#include <cmath>
+#include <iomanip>
 #include <iostream>
 #include <iterator>
 
-namespace Solver{
+#include "random"
 
-[[nodiscard]] int* RandomArrayGenerator(size_t size) {
-    int* array = new int[size];
-    std::random_device r{}; // инициализация std::random_device
-    std::default_random_engine randomEngine(r()); // создание random engine с сидом, сгенерированным r
-    std::uniform_int_distribution distribution(0, 99); // равномерное распределение от 1 до 12
+namespace {
+void PrintArray(int* array, size_t size) {
+    std::cout << "[ ";
+    for (size_t i = 0; i < size; ++i) {
+        std::cout << array[i] << " ";
+    }
+    std::cout << ']';
+}
+
+void PrintResults(Solver::Results results) {
+    std::cout << " || количество перестановок:" << results.countSwap << " || количество сравнений:" << results.countCompare;
+}
+
+void Menu() {
+    std::cout << "\nВыберите работу с массивом:";
+    std::cout << "\n\t1. Статическим";
+    std::cout << "\n\t2. Динамический\n";
+}
+
+void ContinueProgram() {
+    std::cout << "\nПродолжить программу? (y/n)";
+}
+}  // namespace
+
+namespace Solver {
+
+void FillArray(int* array, size_t size) {
+    std::random_device r{};
+    std::default_random_engine randomEngine(r());
+    std::uniform_int_distribution distribution(0, 99);
     for (size_t i = 0; i < size; ++i) {
         array[i] = distribution(randomEngine);
     }
-    return array;
 }
 
-void BubbleSort(int* array, size_t size) {
+[[nodiscard]] Results BubbleSort(int* array, size_t size, bool inverted) {
     bool earlyStopBool = true;
+    int countSwap = 0;
+    int countCompare = 0;
     for (size_t i = 0; i < size - 1; ++i) {
         for (size_t j = i + 1; j < size; ++j) {
-            if (array[i] > array[j]) {
+            if ((array[i] > array[j]) && !inverted) {
                 std::swap(array[i], array[j]);
                 earlyStopBool = false;
+                ++countSwap;
+            } else if ((array[i] < array[j]) && inverted) {
+                std::swap(array[i], array[j]);
+                earlyStopBool = false;
+                ++countSwap;
             }
+            ++countCompare;
         }
         if (earlyStopBool) {
             break;
         }
     }
+    return Results{countCompare, countSwap};
 }
 
-void SelectionSort(int* array, size_t size) {
+[[nodiscard]] Results SelectionSort(int* array, size_t size, bool inverted) {
     int minI = 0;
+    int countSwap = 0;
+    int countCompare = 0;
     for (size_t i = 0; i < size - 1; ++i) {
         minI = i;
         for (size_t j = i + 1; j < size; ++j) {
-            if (array[minI] > array[j]) {
+            if ((array[minI] > array[j]) && !inverted) {
+                minI = j;
+            } else if ((array[minI] < array[j]) && inverted) {
                 minI = j;
             }
+            ++countCompare;
         }
         std::swap(array[i], array[minI]);
+        ++countSwap;
     }
+    return Results{countCompare, countSwap};
 }
 
-void StaticArrayBubbleSort(){
+void StaticArrayMethod() {
     size_t staticArrayN = 10;
+    Results resultsOfSort;
+    int* tempArray = new int[staticArrayN];
+    int* staticArrayBubble = new int[staticArrayN];
+    int* staticArraySelection = new int[staticArrayN];
 
-    int* tempArray[staticArrayN];
-    int* staticArrayBubble[staticArrayN];
-    int* staticArraySelection[staticArrayN];
-
-    tempArray = RandomArrayGenerator(staticArrayN);
-    for (size_t i = 0; i < staticArrayN; ++i){
+    FillArray(tempArray, staticArrayN);
+    for (size_t i = 0; i < staticArrayN; ++i) {
         staticArrayBubble[i] = tempArray[i];
         staticArraySelection[i] = tempArray[i];
     }
 
+    std::cout << "\n---------------МЕТОД ПУЗЫРЬКА---------------";
+    std::cout << "\nисходный массив:                                                  ";
+    PrintArray(staticArrayBubble, staticArrayN);
+    resultsOfSort = BubbleSort(staticArrayBubble, staticArrayN, false);
+    std::cout << "\nсортировка массива методом пузырька по возрастанию:               ";
+    PrintArray(staticArrayBubble, staticArrayN);
+    PrintResults(resultsOfSort);
+    resultsOfSort = BubbleSort(staticArrayBubble, staticArrayN, false);
+    std::cout << "\nсортировка получившегося массива методом пузырька по возрастанию: ";
+    PrintArray(staticArrayBubble, staticArrayN);
+    PrintResults(resultsOfSort);
+    resultsOfSort = BubbleSort(staticArrayBubble, staticArrayN, true);
+    std::cout << "\nсортировка получившегося массива методом пузырька по убыванию:    ";
+    PrintArray(staticArrayBubble, staticArrayN);
+    PrintResults(resultsOfSort);
 
-    for (size_t i = 0; i < staticArrayN; ++i){
-        std::cout << staticArrayBubble[i] << " ";
-    }
-    BubbleSort(&staticArrayBubble, staticArrayN);
-    std::cout << '\n';
-    for (size_t i = 0; i < staticArrayN; ++i){
-        std::cout << staticArraySelection[i] << " ";
-    }
-    std::cout << '\n';
+    std::cout << "\n\n---------------МЕТОД ВЫБОРКИ---------------";
+    std::cout << "\nисходный массив:                                                ";
+    PrintArray(staticArraySelection, staticArrayN);
+    resultsOfSort = SelectionSort(staticArraySelection, staticArrayN, false);
+    std::cout << "\nсортировка массива методом выбора по возрастанию:               ";
+    PrintArray(staticArraySelection, staticArrayN);
+    PrintResults(resultsOfSort);
+    resultsOfSort = SelectionSort(staticArraySelection, staticArrayN, false);
+    std::cout << "\nсортировка получившегося массива методом выбора по возрастанию: ";
+    PrintArray(staticArraySelection, staticArrayN);
+    PrintResults(resultsOfSort);
+    resultsOfSort = SelectionSort(staticArraySelection, staticArrayN, true);
+    std::cout << "\nсортировка получившегося массива методом выбора по убыванию:    ";
+    PrintArray(staticArraySelection, staticArrayN);
+    PrintResults(resultsOfSort);
+
     delete[] tempArray;
     delete[] staticArrayBubble;
     delete[] staticArraySelection;
-
 }
 
-void StartUp(){
-    StaticArrayBubbleSort();
+void DynamicArrayMethod() {
+    size_t dynamicArrayN = 0;
+    Results resultsOfSort;
+
+    std::cout << "Введите длину динамического массива: ";
+    std::cin >> dynamicArrayN;
+
+    int* tempArray = new int[dynamicArrayN];
+    int* dynamicArrayBubble = new int[dynamicArrayN];
+    int* dynamicArraySelection = new int[dynamicArrayN];
+
+    FillArray(tempArray, dynamicArrayN);
+    for (size_t i = 0; i < dynamicArrayN; ++i) {
+        dynamicArrayBubble[i] = tempArray[i];
+        dynamicArraySelection[i] = tempArray[i];
+    }
+
+    std::cout << "\n---------------МЕТОД ПУЗЫРЬКА---------------";
+    std::cout << "\nисходный массив:                                        ";
+    PrintArray(dynamicArrayBubble, dynamicArrayN);
+    resultsOfSort = BubbleSort(dynamicArrayBubble, dynamicArrayN, false);
+    std::cout << "\nсортировка массива методом пузырька по возрастанию:     ";
+    PrintArray(dynamicArrayBubble, dynamicArrayN);
+    PrintResults(resultsOfSort);
+
+    std::cout << "\n\n---------------МЕТОД ВЫБОРКИ---------------";
+    std::cout << "\nисходный массив:                                        ";
+    PrintArray(dynamicArraySelection, dynamicArrayN);
+    resultsOfSort = SelectionSort(dynamicArraySelection, dynamicArrayN, false);
+    std::cout << "\nсортированный массив сортировкой выбора по возрастанию: ";
+    PrintArray(dynamicArraySelection, dynamicArrayN);
+    PrintResults(resultsOfSort);
+
+    delete[] tempArray;
+    delete[] dynamicArrayBubble;
+    delete[] dynamicArraySelection;
 }
 
+void ChooseMethod(char userChoice) {
+    switch (static_cast<UserChoice>(userChoice)) {
+        case UserChoice::staticArray:
+            StaticArrayMethod();
+            break;
+        case UserChoice::dynamicArray:
+            DynamicArrayMethod();
+            break;
+    }
 }
+
+void StartUp() {
+    char choiceToContinue = 'y';
+    char userChoice = '1';
+    while (choiceToContinue == 'y') {
+        Menu();
+        std::cin >> userChoice;
+        ChooseMethod(userChoice);
+        ContinueProgram();
+        std::cin >> choiceToContinue;
+    }
+}
+
+}  // namespace Solver
--- solver.h
+++ solver.h
@@ -2,6 +2,21 @@
 #include <iostream>
 
 namespace Solver {
-[[nodiscard]] int* RandomArrayGenerator(size_t size);
+enum class UserChoice {
+    staticArray = '1',
+    dynamicArray
+};
+struct Results {
+    int countCompare;
+    int countSwap;
+};
+
+[[nodiscard]] Results BubbleSort(int* array, size_t size, bool inverted);
+[[nodiscard]] Results SelectionSort(int* array, size_t size, bool inverted);
+void FillArray(int* array, size_t size);
+
+void StaticArrayMethod();
+void DynamicArrayMethod();
+void ChooseMethod(char userChoice);
 void StartUp();
 }  // namespace Solver
