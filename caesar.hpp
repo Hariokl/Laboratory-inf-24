2f7ff508f818c6b0bd651a8cb9f4c16840be69bd
Hariokl <sevabu06@gmail.com>
2024-12-18 15:12:12 +0300

    asd

--- caesar.hpp
+++ caesar.hpp
@@ -1,5 +1,14 @@
 #pragma once
+#include <iomanip>
 
 namespace Caesar {
+size_t GetTextSize(char* text);
+int GetTextWordSize(char* text);
+char* ReadFile(const char* filename);
+void WriteFile(const char* filename, char* encodedMessage);
+char ShiftChar(char character);
+char ReverseShiftChar(char character);
+char* Encode(char* sourceMessage);
+char* Decode(char* encodedMessage);
 void StartUp();
-}
+}  // namespace Caesar
