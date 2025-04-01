18082cbf0a973847b10672c79416cb0128edd43d
Hariokl <sevabu06@gmail.com>
2025-04-01 19:44:24 +0300

    A

--- project1/myobject/myobject.cpp
+++
@@ -1,7 +0,0 @@
-#include "myobject.hpp"
-
-namespace myobject {
-    int product(int a, int b) {
-        return a * b;
-    }
-}
--- project2/myobject.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef MYOBJECT_H
-#define MYOBJECT_H
-
-namespace myobject {
-    int product(int a, int b);
-}
-
-#endif //MYMATH_H
